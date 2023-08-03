14dd0e750d25786758ca74c7c5ec7fb52858ccd6
Xing Wei <iwishing@163.com>
2023-08-03 18:17:47 +0800

    UUID

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='6801f352-5858-4079-bab7-71f20749391e'
+UUID='75b5066e-2f45-4a53-a8c7-888766557fb8'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
